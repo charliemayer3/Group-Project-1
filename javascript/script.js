28f0b68933a7e69a0e775bd39700026b021114bb
Charlie Mayer <charliemayer3@gmail.com>
2018-02-07 13:33:36 -0700

    tied in css, fixed other things

--- javascript/script.js
+++ javascript/script.js
@@ -13,20 +13,20 @@ $(document).ready(function() {
 		// };
 		// firebase.initializeApp(config);
 
-		var roverPhotosURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&page=1&api_key=NmKeMjPtnfZwr62CCkpaHzcy3OVNrb1v0Wi9RB1M";
+		// var roverPhotosURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&page=1&api_key=NmKeMjPtnfZwr62CCkpaHzcy3OVNrb1v0Wi9RB1M";
 
-		$.ajax({
-			url: roverPhotosURL,
-			method: "GET"
-		}).then(function(response) {
-			console.log(response);
-			for (var i = 0; i < response.photos.length; i++) {
-				var pictureURL = response.photos[i].img_src;
-				// console.log(pictureURL)
-				var roverImage = $("<img>").attr("src", pictureURL);
-				// $("#map").append(roverImage);
-				}
-		})
+		// $.ajax({
+		// 	url: roverPhotosURL,
+		// 	method: "GET"
+		// }).then(function(response) {
+		// 	console.log(response);
+		// 	for (var i = 0; i < response.photos.length; i++) {
+		// 		var pictureURL = response.photos[i].img_src;
+		// 		// console.log(pictureURL)
+		// 		var roverImage = $("<img>").attr("src", pictureURL);
+		// 		// $("#map").append(roverImage);
+		// 		}
+		// })
 
 		var map;
 		var issNowURL = "http://api.open-notify.org/iss-now.json";
@@ -50,13 +50,12 @@ $(document).ready(function() {
 					map: map,
 					icon: markerImage
 				});
-				// initMap(latitude, long)
+				$("#latBox").html("Current Latitude and Longitude that the ISS is above. <br> Latitude: " + latitude + ", Longitude: " + long);
 			})
 		}
 
 		setInterval(getCoords, 3000)
 
-		// console.log(latitude)
 		function initMap() {
 			$.ajax({    
 				url: issNowURL,
@@ -80,11 +79,8 @@ $(document).ready(function() {
 					map: map,
 					icon: markerImage
 				});
+				$("#latBox").html("Current Latitude and Longitude that the ISS is above. <br> Latitude: " + latitude + ", Longitude: " + long);
 			})
-
-		// console.log(typeof lat)
-		// console.log(typeof long)
-
 		}
 
 		initMap()
@@ -95,7 +91,10 @@ $(document).ready(function() {
 		  url: inSpaceURL,
 		  method: "GET"
 		}).then(function(response) {
-		  console.log(response);
+		  	console.log(response);
+		  	var numOfPeople = response.number;
+		  	$("#issText1").text("The International Space Station (ISS) is a space station, or a habitable artificial satellite, in low Earth orbit. Its first component launched into orbit in 1998, the last pressurised module was fitted in 2011, and the station is expected to be used until 2028. Development and assembly of the station continues, with components scheduled for launch in 2018 and 2019. The ISS is the largest human-made body in low Earth orbit and can often be seen with the naked eye from Earth. The ISS consists of pressurised modules, external trusses, solar arrays, and other components. ISS components have been launched by Russian Proton and Soyuz rockets, and American Space Shuttles.")
+			$("#issText2").text("The ISS serves as a microgravity and space environment research laboratory in which crew members conduct experiments in biology, human biology, physics, astronomy, meteorology, and other fields. The station is suited for the testing of spacecraft systems and equipment required for missions to the Moon and Mars. The ISS maintains an orbit with an altitude of between 330 and 435 km (205 and 270 mi) by means of reboost manoeuvres using the engines of the Zvezda module or visiting spacecraft. It completes 15.54 orbits per day. There are currently " + numOfPeople + " people aboard the station.")
 		})
 
 		var api_key = "xY06mF2u9EK3PyQ1DVmfelmYhIuDhNmePy05t9kn"
@@ -110,8 +109,10 @@ $(document).ready(function() {
           	var nasaHeader = $("<h2 id='apodHeader'>Today's Astronomy Picture of the Day</h2>");
           	var nasaTitle = $("<div>" + response.title + "</div>");
           	var nasaImage = $("<img src='" + response.url + "' width=500px height=500px id='apodImage'>");
-          	var nasaExplanation = $("<p id='nasaText'>" + response.explanation + "</p>");
-          	$("#nasaInfo").append(nasaHeader, nasaTitle, nasaImage, nasaExplanation);
+          	$("#nasaHeader").append(nasaHeader)
+          	$("#nasaText").text(response.explanation);
+          	$("#nasaInfo").append(nasaTitle, nasaImage);
+          	
       	})
 	
 })
